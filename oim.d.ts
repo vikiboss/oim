6c3723d93b5d8557eb0ee6f2c6778ddc8086b64d
Viki <vikiboss@qq.com>
2021-12-14 21:54:14 +0800

    🐛 fix type error

--- oim.d.ts
+++ oim.d.ts
@@ -1,11 +1,11 @@
 export function chunk<T>(array: Array<T>, size?: number): Array<Array<T>>;
 export function format(date?: Date, formatStr?: string): string;
 export function formatDateDiff(millisecond: number, isZh?: boolean): string;
-export function left(string: string, sub: string, greed: boolean): string;
+export function left(string: string, sub: string, greed?: boolean): string;
 export function mid(string: string, start: string, end: string, greed?: boolean): string;
 export function rand<T>(array: Array<T>): Array<T> | T;
 export function random(min?: number, max?: number): number;
-export function right(string: string, sub: string, greed: boolean): string;
+export function right(string: string, sub: string, greed?: boolean): string;
 export function shuffle(array: Array<any>): Array<any>;
 export function wait(ms: number): Promise<undefined>;
 
